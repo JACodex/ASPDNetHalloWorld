886cd979a7b4e85e0a486f05aa29a9ec2e52b3fc
Joshua Lynch <joshua.lynch@hotmail.com>
2022-05-04 14:16:06 -0400

    added route parameters

--- Controllers/HelloController1.cs
+++ Controllers/HelloController1.cs
@@ -10,7 +10,10 @@ namespace HelloASPDnetWorld.Controllers
         [Route("/waddupworld/")]
         public IActionResult Index()
         {
-            string html = "<h1>Waddup World</h1>";
+            string html = "<h1>Waddup World</h1>"+
+                "<form method='post' action='/waddupworld'>" +
+                "<input type='text' name='name'> " +
+                "<input type='submit' value='enter a name'> </form>";
             Console.WriteLine("HelloController endpoint hit");
             return Content(html, "text/html");
         }
@@ -22,74 +25,75 @@ namespace HelloASPDnetWorld.Controllers
             return Content(results, "text/html");
 
         }
+        [HttpPost("/waddupworld/{name?}")]
+        // GET: HelloController1/Details/5
+        public ActionResult Details(string name)
+        {
+            string html = $"<h1 style='color: red'>{name}</h1>";
+            return Content(html, "text/html");
+        }
 
-    //    // GET: HelloController1/Details/5
-    //    public ActionResult Details(int id)
-    //    {
-    //        return View();
-    //    }
+        //    // GET: HelloController1/Create
+        //    public ActionResult Create()
+        //    {
+        //        return View();
+        //    }
 
-    //    // GET: HelloController1/Create
-    //    public ActionResult Create()
-    //    {
-    //        return View();
-    //    }
+        //    // POST: HelloController1/Create
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Create(IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
 
-    //    // POST: HelloController1/Create
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public ActionResult Create(IFormCollection collection)
-    //    {
-    //        try
-    //        {
-    //            return RedirectToAction(nameof(Index));
-    //        }
-    //        catch
-    //        {
-    //            return View();
-    //        }
-    //    }
+        //    // GET: HelloController1/Edit/5
+        //    public ActionResult Edit(int id)
+        //    {
+        //        return View();
+        //    }
 
-    //    // GET: HelloController1/Edit/5
-    //    public ActionResult Edit(int id)
-    //    {
-    //        return View();
-    //    }
+        //    // POST: HelloController1/Edit/5
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Edit(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
 
-    //    // POST: HelloController1/Edit/5
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public ActionResult Edit(int id, IFormCollection collection)
-    //    {
-    //        try
-    //        {
-    //            return RedirectToAction(nameof(Index));
-    //        }
-    //        catch
-    //        {
-    //            return View();
-    //        }
-    //    }
+        //    // GET: HelloController1/Delete/5
+        //    public ActionResult Delete(int id)
+        //    {
+        //        return View();
+        //    }
 
-    //    // GET: HelloController1/Delete/5
-    //    public ActionResult Delete(int id)
-    //    {
-    //        return View();
-    //    }
-
-    //    // POST: HelloController1/Delete/5
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public ActionResult Delete(int id, IFormCollection collection)
-    //    {
-    //        try
-    //        {
-    //            return RedirectToAction(nameof(Index));
-    //        }
-    //        catch
-    //        {
-    //            return View();
-    //        }
-    //    }
-}
+        //    // POST: HelloController1/Delete/5
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Delete(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
+    }
 }
